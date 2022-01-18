a9154c0cea262113a8a7dfc6b5a7c540480fdcf6
Gil Shteyman <gilshteyman@gils-air.wifi.local.cmu.edu>
2022-01-18 15:28:28 -0500

    color gradient

--- display.py
+++ display.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+import time 
 
 window = tk.Tk()
-
+inp = 0
 while(True):
-    inp = 3
+    time.sleep(.5)
+    inp += 1
+    inp = inp % 18
+    print(inp)
     temp = '#00' + str(hex(int(inp) * 14)) + '00'
     temp = temp[0:3] + temp[5:]
+    if len(temp) == 6:
+        temp = temp[0] + '0' + temp[1:]
+        
     window['bg'] = temp
+    print(temp)
     window.attributes('-fullscreen', True)
-    window.mainloop()
+    window.update()
+window.mainloop()
